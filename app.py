eae7476a4d8de93f8d112bbf4ef9bd433f83706b
camilosarra <camilosarra@hotmail.com>
2026-03-02 14:58:49 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,77 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+import socketio
+import os
+
+sio = socketio.AsyncServer(async_mode="asgi", cors_allowed_origins="*")
+fastapi_app = FastAPI()
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
+
+fastapi_app.mount("/static", StaticFiles(directory=os.path.join(BASE_DIR, "static")), name="static")
+
+cola_turnos = []
+contador_turnos = 0
+atendiendo = None
+
+@fastapi_app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@fastapi_app.get("/admin", response_class=HTMLResponse)
+async def admin(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@sio.event
+async def connect(sid, environ):
+    await enviar_estado(sid)
+
+@sio.event
+async def solicitar_turno(sid, data):
+    global contador_turnos
+    contador_turnos += 1
+    turno = {
+        "id": contador_turnos,
+        "nombre": data["nombre"],
+        "tema": data["tema"],
+        "sid": sid
+    }
+    cola_turnos.append(turno)
+    await sio.emit("turno_asignado", {"id": turno["id"]}, to=sid)
+    await enviar_estado()
+
+@sio.event
+async def llamar_turno(sid, turno_id):
+    global atendiendo
+    turno_llamado = None
+
+    for t in cola_turnos:
+        if t["id"] == turno_id:
+            turno_llamado = t
+            break
+
+    if turno_llamado:
+        atendiendo = turno_llamado["nombre"]
+        cola_turnos.remove(turno_llamado)
+        await sio.emit("turno_llamado", {"id": turno_llamado["id"]}, to=turno_llamado["sid"])
+
+    await enviar_estado()
+
+async def enviar_estado(sid=None):
+    data = {
+        "cola": [
+            {"id": t["id"], "nombre": t["nombre"], "tema": t["tema"]}
+            for t in cola_turnos
+        ],
+        "atendiendo": atendiendo
+    }
+
+    if sid:
+        await sio.emit("estado", data, to=sid)
+    else:
+        await sio.emit("estado", data)
+
+app = socketio.ASGIApp(sio, other_asgi_app=fastapi_app)
